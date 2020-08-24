4b3d660a136c7270f695d96c46ab1d9ec6c7c817
wentjiang <wentjiang@sina.com>
2020-08-24 22:31:05 +0800

    ADD TICKET Calculate method and test

--- src/main/java/cn/xpbootcamp/gildedrose/CommonProductCalculator.java
+++ src/main/java/cn/xpbootcamp/gildedrose/CommonProductCalculator.java
@@ -1,5 +1,8 @@
 package cn.xpbootcamp.gildedrose;
 
+/**
+ * @author wentao.jiang
+ */
 public class CommonProductCalculator {
     public static final int MAX_QUALITY = 50;
     public static final int MIN_QUALITY = 0;
@@ -8,29 +11,28 @@ public class CommonProductCalculator {
     public static final int COMMON_PRODUCT_DOUBLE_REDUCE_EVERY_DAY = COMMON_PRODUCT_REDUCE_EVERY_DAY * 2;
 
     public int calculateCommonProductQuality(int sellIn, int quality, int updatedSellIn) {
-        int commonQuality = getProductSellInQuality(sellIn, quality);
         int updatedQuality;
-        int after_days = sellIn - updatedSellIn;
-        int afterUpdateSellIn = sellIn - after_days;
-        if (afterUpdateSellIn > 0) {
-            updatedQuality = commonQuality + afterUpdateSellIn * COMMON_PRODUCT_REDUCE_EVERY_DAY;
+        int afterDays = sellIn - updatedSellIn;
+        int afterUpdateSellIn = sellIn - afterDays;
+        if (sellIn > 0) {
+            //当前保质期大于0的情况
+            if (afterUpdateSellIn > 0) {
+                int needReduceQuality = afterDays * COMMON_PRODUCT_REDUCE_EVERY_DAY;
+                updatedQuality = quality - needReduceQuality;
+            } else {
+                int needReduceQuality = sellIn * COMMON_PRODUCT_REDUCE_EVERY_DAY + afterUpdateSellIn * COMMON_PRODUCT_DOUBLE_REDUCE_EVERY_DAY;
+                updatedQuality = quality - needReduceQuality;
+            }
         } else {
-            updatedQuality = commonQuality + afterUpdateSellIn * COMMON_PRODUCT_DOUBLE_REDUCE_EVERY_DAY;
+            //当前保质期小于0的情况
+            if (afterUpdateSellIn > 0) {
+                int needReduceQuality = -sellIn * COMMON_PRODUCT_DOUBLE_REDUCE_EVERY_DAY - afterUpdateSellIn * COMMON_PRODUCT_REDUCE_EVERY_DAY;
+                updatedQuality = quality - needReduceQuality;
+            } else {
+                int needReduceQuality = afterDays * COMMON_PRODUCT_DOUBLE_REDUCE_EVERY_DAY;
+                updatedQuality = quality - needReduceQuality;
+            }
         }
         return Math.min(Math.max(MIN_QUALITY, updatedQuality), MAX_QUALITY);
     }
-
-    /**
-     * 获取商品到保质期的quality
-     *
-     * @return 到保质期的quality
-     */
-    public int getProductSellInQuality(int sellIn, int quality) {
-        if (sellIn > 0) {
-            return quality - (sellIn * COMMON_PRODUCT_REDUCE_EVERY_DAY);
-        } else {
-            return quality + Math.abs(sellIn) * COMMON_PRODUCT_REDUCE_EVERY_DAY * 2;
-        }
-    }
-
 }
--- src/main/java/cn/xpbootcamp/gildedrose/TicketCalculator.java
+++ src/main/java/cn/xpbootcamp/gildedrose/TicketCalculator.java
@@ -1,5 +1,8 @@
 package cn.xpbootcamp.gildedrose;
 
+/**
+ * @author wentao.jiang
+ */
 public class TicketCalculator {
 
     public static final int MAX_QUALITY = 50;
@@ -7,36 +10,49 @@ public class TicketCalculator {
 
     public static final int LESS_5_EVERY_DAY_ADD = 3;
     public static final int LESS_10_EVERY_DAY_ADD = 2;
-    public static final int COMMON_EVERY_DAY_ADD = 1;
+    public static final int MORE_10_EVERY_DAY_ADD = 1;
 
     public static final int DAY_SPLIT_0 = 0;
     public static final int DAY_SPLIT_5 = 5;
     public static final int DAY_SPLIT_10 = 10;
 
     public int calculateBackstagePassQuality(int sellIn, int quality, int updatedSellIn) {
+
         int updatedQuality = 0;
         int afterDays = sellIn - updatedSellIn;
+        //更新后的时间具体过期的天数
         int afterUpdateSellIn = sellIn - afterDays;
-        if (sellIn <= DAY_SPLIT_0) {
+        if (sellIn <= DAY_SPLIT_0 || afterUpdateSellIn < DAY_SPLIT_0) {
             //初始天数为负的情况,无法计算其他值
-
             updatedQuality = MIN_QUALITY;
         } else if (sellIn <= DAY_SPLIT_5) {
             //当前天数在5以内的时候
-            if (afterUpdateSellIn <= DAY_SPLIT_0) {
-                updatedQuality = MIN_QUALITY;
-            } else if (afterUpdateSellIn <= DAY_SPLIT_5) {
+            if (afterUpdateSellIn <= DAY_SPLIT_5) {
                 int needIncreaseQuality = afterDays * LESS_5_EVERY_DAY_ADD;
                 updatedQuality = quality + needIncreaseQuality;
-            } else if (afterUpdateSellIn <= DAY_SPLIT_10) {
-                int needIncreaseQuality = -(DAY_SPLIT_5 - sellIn) * LESS_5_EVERY_DAY_ADD - (sellIn - updatedSellIn - DAY_SPLIT_5) * LESS_10_EVERY_DAY_ADD;
+            }
+        } else if (sellIn <= DAY_SPLIT_10) {
+            //当前天数在5以内的时候
+            if (afterUpdateSellIn <= DAY_SPLIT_5) {
+                int needIncreaseQuality = (DAY_SPLIT_5 - updatedSellIn) * LESS_5_EVERY_DAY_ADD + (sellIn - DAY_SPLIT_5) * LESS_10_EVERY_DAY_ADD;
                 updatedQuality = quality + needIncreaseQuality;
-            } else {
-                int needIncreaseQuality = -(DAY_SPLIT_5 - sellIn) * LESS_5_EVERY_DAY_ADD - (DAY_SPLIT_10 - DAY_SPLIT_5) * LESS_10_EVERY_DAY_ADD - (sellIn - updatedSellIn - DAY_SPLIT_10) * COMMON_EVERY_DAY_ADD;
+            } else if (afterUpdateSellIn <= DAY_SPLIT_10) {
+                int needIncreaseQuality = (sellIn - updatedSellIn) * LESS_10_EVERY_DAY_ADD;
                 updatedQuality = quality + needIncreaseQuality;
             }
-        } else if (sellIn <= 10) {
-
+        } else {
+            //当前天数在5以内的时候
+            if (afterUpdateSellIn <= DAY_SPLIT_5) {
+                int needIncreaseQuality = (sellIn - updatedSellIn) * MORE_10_EVERY_DAY_ADD
+                        + (DAY_SPLIT_10 - DAY_SPLIT_5) * LESS_10_EVERY_DAY_ADD + (DAY_SPLIT_5 - afterUpdateSellIn) * LESS_5_EVERY_DAY_ADD;
+                updatedQuality = quality + needIncreaseQuality;
+            } else if (afterUpdateSellIn <= DAY_SPLIT_10) {
+                int needIncreaseQuality = (sellIn - updatedSellIn) * MORE_10_EVERY_DAY_ADD + (DAY_SPLIT_10 - afterUpdateSellIn) * LESS_10_EVERY_DAY_ADD;
+                updatedQuality = quality + needIncreaseQuality;
+            } else {
+                int needIncreaseQuality = (sellIn - afterUpdateSellIn) * MORE_10_EVERY_DAY_ADD;
+                updatedQuality = quality + needIncreaseQuality;
+            }
         }
         return Math.min(Math.max(MIN_QUALITY, updatedQuality), MAX_QUALITY);
     }
